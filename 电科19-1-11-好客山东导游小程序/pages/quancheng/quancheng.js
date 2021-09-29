467bf06508dd3b3b9212be033bdcfcfac71c65db
Lorenzo273863 <85985633+Lorenzo273863@users.noreply.github.com>
2021-09-29 20:30:16 +0800

    Add files via upload

---
+++ 电科19-1-11-好客山东导游小程序/pages/quancheng/quancheng.js
@@ -0,0 +1,7 @@
+// pages/quancheng/quancheng.js
+Page({
+
+  data: {
+      imgSrcjinan1:"/images/jinan1.png",
+  },
+})
---
+++ 电科19-1-11-好客山东导游小程序/pages/sankong/sankong.js
@@ -0,0 +1,11 @@
+// pages/sankong/sankong.js
+Page({
+
+  data: {
+    imgSrcsankong1:"/images/sankong1.png",
+    imgSrcsankong2:"/images/sankong2.png",
+    imgSrcsankong3:"/images/sankong3.png"
+  },
+
+  
+})
